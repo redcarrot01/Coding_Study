5e8665f95b09a0554bb8b5b5a73ae9affff0ddbb
redcarrot01 <38436013+redcarrot01@users.noreply.github.com>
2020-10-08 23:08:27 +0900

    Update day1008.py

--- Month10/day1008.py
+++ Month10/day1008.py
@@ -1,19 +1,25 @@
-def solution(n, time):
-    left = 1
-    right = max(time)*n
+def solution(n, times):
+    low =1
+    high = max(times)*n
     answer = 0
-    while left <= right :
-        mid = (left + right)//2
+    
+    while low <= high:
+        # 한 심사관에게 주어진 시간
+        mid = (low+high) //2
         people = 0
-        for i in time :
+        for i in times:
+            # 각 심사관 마다 몇명 가능한지
             people += mid//i
-            if people >= n :
+            # 모든 사람이 심사 가능 하다면 벗어나기
+            if people >= n:
                 break
-
-        if people >= n :
+        # 시간 줄여보기        
+        if people >=n:
             answer = mid
-            right = mid -1
-        else :
-            left = mid + 1
-
+            high = mid -1
+        # 모든 사람이 심사 할 수 없는 경우 시간 늘리기    
+        else:
+            low = mid + 1
+    
+    
     return answer
